f4500a977e2fbda771925e94556b5fdd783d057c
SunJX <sun.23@outlook.com>
2017-04-22 17:01:26 +0800

    connect to sql

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+	//error_reporting(E_ALL ^ E_DEPRECATED);
+	header("Content-type: text/html; charset=utf-8");
+	define('HOST','127.0.0.1');
+	define('USERNAME','root');
+	define('PASSWORD','123');
+	define('DB','xunhui');
+?>
